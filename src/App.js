44fcae902cb325280a43a7762ca1112c05acc3eb
savin-dev <niduwarafonseka@gmail.com>
2021-01-02 01:05:21 +0530

    run Hello world

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div><h1>hello savin and tharusha!!!!!</h1><h2>Sri lanka</h2></div>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div className="App">
+      <Login/>
+    </div>
   );
 }
 
