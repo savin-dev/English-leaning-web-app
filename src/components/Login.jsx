adc49c1b02df9c9db0e5f2af941f5cf9abf39098
savin-dev <niduwarafonseka@gmail.com>
2021-01-02 01:30:27 +0530

    create the first stage component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+        name : 'Savin',
+        girl : 'Oneli'
+        }    
+    }
     render() {
         return (
             <div>
-                <h1>Hello world</h1>
+                <h1>{this.state.name} loves {this.state.girl}</h1>
             </div>
-        )
+        );
     }
 }
 
