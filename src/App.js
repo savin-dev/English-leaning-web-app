1181f162411fa73bfec05b7f95c5f18ebf6b6765
savin-dev <niduwarafonseka@gmail.com>
2021-01-02 00:22:31 +0530

    setup react

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div><h1>hello savin and tharusha!!!!!</h1><h2>Sri lanka</h2></div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
