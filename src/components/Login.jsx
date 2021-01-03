f20064c92378fa063d8776554c4c26051ae5b048
savin-dev <niduwarafonseka@gmail.com>
2021-01-03 12:00:37 +0530

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -20,7 +20,7 @@ export class Login extends Component {
                     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossOrigin="anonymous" />
                     {/*Custom styles*/}
                     <link rel="stylesheet" type="text/css" href="styles.css" />
-                    <h1>page</h1>
+                    <h1> -</h1>
                         <div className="container">
                             <div className="d-flex justify-content-center h-100">
                                 <div className="card">
