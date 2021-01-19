050f3b58e9e9472496707610506e3723e0ddbaa3
savin-dev <74615072+savin-dev@users.noreply.github.com>
2021-01-19 11:12:14 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,12 +13,8 @@ export class Login extends Component {
             <div className="container">
                 <div>
                     <title>Login Page</title>
-                    {/*Made with love by Mutiullah Samim */}
-                    {/*Bootsrap 4 CDN*/}
                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous" />
-                    {/*Fontawesome CDN*/}
                     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossOrigin="anonymous" />
-                    {/*Custom styles*/}
                     <link rel="stylesheet" type="text/css" href="styles.css" />
                     <h1> -</h1>
                         <div className="container">
